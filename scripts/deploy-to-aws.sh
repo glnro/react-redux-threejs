f3f221bf817e062ee61b7f3d282a88c108927c19
glnro <lg@glnro.com>
2017-03-06 10:09:47 -0500

    Remove aws deploy from pipeline]

--- scripts/deploy-to-aws.sh
+++ scripts/deploy-to-aws.sh
@@ -1,6 +1,5 @@
-#!bin/bash
-
 ssh -i $AWS_KEY $AWS_BOX
-docker pull lgallinaro/rr3js
-docker build -t lgallinaro/rr3js
-docker run lgallinaro/rr3js
+docker ps -a
+#docker pull lgallinaro/rr3js
+#docker build -t lgallinaro/rr3js .
+docker run -i -t lgallinaro/rr3js
