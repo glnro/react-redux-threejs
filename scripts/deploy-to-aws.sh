1931beebf76893c82bd244e045eda8daacfd2f40
glnro <lg@glnro.com>
2017-03-08 21:29:33 -0500

    Remove boilerplate js server

--- scripts/deploy-to-aws.sh
+++ scripts/deploy-to-aws.sh
@@ -1,5 +1,4 @@
 ssh -i $AWS_KEY $AWS_BOX
 docker ps -a
 #docker pull lgallinaro/rr3js
-#docker build -t lgallinaro/rr3js .
-docker run -i -t lgallinaro/rr3js
+docker run -i -t -p 80:3001 lgallinaro/rr3js
